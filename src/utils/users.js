234b531ccef36da0223707a17b0f02e147e293dd
Aditya Vatsa <adityavatsa08@gmail.com>
2020-07-18 15:49:42 +0530

    chat app finished

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,48 +1,48 @@
 const users = []
 
-const addUser = ({id, username, room}) => {
+const addUser = ({ id, username, room }) => {
     // Clean the data
     username = username.trim().toLowerCase()
     room = room.trim().toLowerCase()
 
-    // validate the data
-    if(!username || !room){
-        return{
-            error: 'username and room are required'
+    // Validate the data
+    if (!username || !room) {
+        return {
+            error: 'Username and room are required!'
         }
     }
+
     // Check for existing user
     const existingUser = users.find((user) => {
         return user.room === room && user.username === username
     })
 
-    // validate username
-    if(existingUser){
+    // Validate username
+    if (existingUser) {
         return {
-            error: 'username is in use!'
+            error: 'Username is in use!'
         }
     }
 
-    // store user
-    const user = {id, username ,room}
+    // Store user
+    const user = { id, username, room }
     users.push(user)
-    return {user}
+    return { user }
 }
 
 const removeUser = (id) => {
     const index = users.findIndex((user) => user.id === id)
-    if(index !== 1){
-        return users.splice(index, 1)
+
+    if (index !== -1) {
+        return users.splice(index, 1)[0]
     }
 }
 
 const getUser = (id) => {
-    const index = users.findIndex((user) => user.id === id)
-    if(index !==1){
-        return users[index]
-    }
+    return users.find((user) => user.id === id)
 }
-const getUsersInRoom = (room) =>{
+
+const getUsersInRoom = (room) => {
     room = room.trim().toLowerCase()
     return users.filter((user) => user.room === room)
 }
